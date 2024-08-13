01b4901a4a326801e7f4a6eee1eec74bea57c387
Mario Paz <mariopaz5399@gmail.com>
2024-08-13 14:07:01 -0600

    agregar iconos al menu principal y las imagenes ala carpeta de assets

--- fluttter_examen1_mariopaz/lib/pantallas/screens/menu_principal.dart
+++ fluttter_examen1_mariopaz/lib/pantallas/screens/menu_principal.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+
 class MenuPrincipalScreen extends StatelessWidget {
   const MenuPrincipalScreen({Key? key}) : super(key: key);
 
